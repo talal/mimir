65ea02c72aaba542977082e8679993ae27c3dafe
Muhammad Talal Anwar <talalanwar@outlook.com>
2019-04-16 22:22:57 +0200

    improve logic for OpenStack cloud info retrieval

--- pkg/prompt/cloud.go
+++ pkg/prompt/cloud.go
@@ -7,26 +7,28 @@ import (
 	"github.com/talal/go-bits/color"
 )
 
-// getOSCloud returns the current OpenStack cloud info using the
-// "CURRENT_OS_CLOUD" environment variable, if set. If not, then individual
-// OpenStack environment variables are used to get the cloud info.
+// getOSCloud returns the current OpenStack cloud info using the standard
+// OpenStack environment variables.
+//
+// If the 'CURRENT_OS_CLOUD' environment variable is defined then its value is
+// returned instead.
 func getOSCloud() string {
-	cloudInfo := os.Getenv("CURRENT_OS_CLOUD")
-	if cloudInfo == "" {
-		osRegion := getOSEnvVal("OS_REGION_NAME", "")
-		osUser := getOSEnvVal("OS_USERNAME", "")
-		osUserDomain := getOSEnvVal("OS_USER_DOMAIN_NAME", "OS_USER_DOMAIN_ID")
-		osProject := getOSEnvVal("OS_PROJECT_NAME", "OS_PROJECT_ID")
-		osProjectDomain := getOSEnvVal("OS_PROJECT_DOMAIN_NAME", "OS_PROJECT_DOMAIN_ID")
-
-		// at least one value should be available
-		if osRegion != "" || osUser != "" || osUserDomain != "" ||
-			osProject != "" || osProjectDomain != "" {
-			cloudInfo = fmt.Sprintf("%s/%s@%s/%s@%s", osRegion, osUser, osUserDomain,
-				osProject, osProjectDomain)
-		}
+	if getBoolEnv("MIMIR_DISABLE_CLOUD") {
+		return ""
 	}
-	if cloudInfo == "" {
+
+	if cloud := os.Getenv("CURRENT_OS_CLOUD"); cloud != "" {
+		return color.Sprintf(color.Magenta, cloud)
+	}
+
+	cloudInfo := fmt.Sprintf("%s/%s@%s/%s@%s",
+		getOSEnvVal("OS_REGION_NAME", ""),
+		getOSEnvVal("OS_USERNAME", ""),
+		getOSEnvVal("OS_USER_DOMAIN_NAME", "OS_USER_DOMAIN_ID"),
+		getOSEnvVal("OS_PROJECT_NAME", "OS_PROJECT_ID"),
+		getOSEnvVal("OS_PROJECT_DOMAIN_NAME", "OS_PROJECT_DOMAIN_ID"))
+
+	if cloudInfo == "/@/@" {
 		return ""
 	}
 
@@ -36,11 +38,11 @@ func getOSCloud() string {
 // getOSEnvVal takes two keys for OpenStack environment variables and returns
 // the corresponding value for the first key, if no value exists, then the
 // corresponding value for the second key is returned.
-func getOSEnvVal(key1, key2 string) string {
-	var val string
-	if val = os.Getenv(key1); val == "" {
+func getOSEnvVal(key1, key2 string) (val string) {
+	val = os.Getenv(key1)
+	if val == "" {
 		val = os.Getenv(key2)
 	}
 
-	return val
+	return
 }
